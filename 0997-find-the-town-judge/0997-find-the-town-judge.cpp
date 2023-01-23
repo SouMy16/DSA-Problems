876084ec641a137b992dd4235336751141f6e2df
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-01-23 10:28:41 +0530

    Time: 194 ms (75.36%), Space: 60.7 MB (86.87%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> a(n+1,0),b(n+1,0);
+        
+            for(int i=0;i<trust.size();i++)
+            {
+                    a[trust[i][1]]++;
+                    b[trust[i][0]]++;
+            }
+            for(int i=1;i<=n;i++){
+                if(a[i]==n-1 && b[i]==0) return i;
+            }
+            return -1;
+        
+    }
+};
