caf06513a54d3870ea197f479a62b2d6a381285e
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-02-24 16:19:43 +0530

    Time: 593 ms (46.61%), Space: 56.6 MB (98.31%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -2,62 +2,25 @@ class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
         long long int n = nums.size();
-        priority_queue<long long int> pq1;
-        priority_queue<long long int, vector<long long int>, greater<long long int>> pq2;
+        priority_queue<int> pq;
+        int mn = INT_MAX;
+        int ans = INT_MAX;
         for(int i=0; i<n; i++)
         {
-            pq1.push(nums[i]);
-            pq2.push(nums[i]);
+            if(nums[i]%2 == 1) nums[i]*=2;
+            pq.push(nums[i]);
+            mn = min(mn, nums[i]);
         }
-        long long int mx = pq1.top(), mn = pq2.top();
-        long long int ans = mx-mn;
-        cout<<mx<<" "<<mn<<" "<<n<<'\n';
-        if(mn%2 == 0)
+        while(pq.top()%2 == 0)
         {
-            if(mx%2 == 1) ans = min(ans, mx-mn);
-            else{
-                while(!pq1.empty() && pq1.top()%2==0)
-                {
-                    long long int x = pq1.top();
-                    pq1.pop();
-                    pq1.push(x/2);
-                    pq2.push(x/2);
-                    ans = min(ans, abs(pq1.top()-pq2.top()));
-                }
-            }
+            int x = pq.top();
+            pq.pop();
+            x /= 2;
+            pq.push(x);
+            mn = min(mn, x);
+            ans = min(ans, pq.top()-mn);
         }
-        else if(mx%2 == 1)
-        {
-            while(!pq2.empty() && pq2.top()%2==1)
-            {
-                long long int x = pq2.top();
-                pq2.pop();
-                pq2.push(x*2);
-                pq1.push(x*2);
-                ans = min(ans, abs(pq1.top()-pq2.top()));
-            }
-        }
-        else{
-            // mn is odd and mx is even
-            while(pq2.top()%2==1)
-            {
-                long long int x = pq2.top();
-                x *= 2;
-                pq2.pop();
-                pq2.push(x);
-                pq1.push(x);
-                ans = min(ans, abs(pq1.top()-pq2.top()));
-            }
-            while(pq1.top()%2==0)
-            {
-                long long int x = pq1.top();
-                x /= 2;
-                pq1.pop();
-                pq1.push(x);
-                pq2.push(x);
-                ans = min(ans, abs(pq1.top()-pq2.top()));
-            }
-        }
+        ans = min(ans, pq.top()-mn);
         return ans;
     }
 };
