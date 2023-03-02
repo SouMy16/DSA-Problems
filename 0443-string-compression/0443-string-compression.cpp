e1322604cb36166ad42f840e8f5cb18d7dde3197
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-03-02 23:50:54 +0530

    Time: 5 ms (68.76%), Space: 9 MB (42.12%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
 	int compress(vector<char>& chars) {
-		if(chars.size() < 2)
-			return chars.size();
-
-		int  i = 0;
-		int  j = 0;
-
-		while(i < chars.size()) {
-			chars[j] = chars[i];
-			int cnt = 0;
-			while(i < chars.size() && chars[i] == chars[j]) {
-				cnt++;
-				i++;
-			}
-
-			if(cnt == 1) {
-				j++;
-			} else {
-				string str = to_string(cnt);
-				for(auto ch: str)
-					chars[++j] = ch;
-				j++;
-			}
-		}
-
-		return j;
+		int ans = 1;
+        int c = 1;
+        if(chars.size() < 2) return chars.size();
+        for(int i=1; i<chars.size(); i++)
+        {
+            if(chars[i] != chars[i-1])
+            {
+                if(c == 1)
+                {
+                    chars[ans] = chars[i];
+                    ans++;
+                }
+                else{
+                    string x = to_string(c);
+                    for(int i=0; i<x.size(); i++)
+                    {
+                        chars[ans] = x[i];
+                        ans++;
+                    }
+                    chars[ans] = chars[i];
+                    ans++;
+                }
+                c = 1;
+            }
+            else c++;
+        }
+        if(c>1)
+        {
+            string x = to_string(c);
+            for(int i=0; i<x.size(); i++)
+            {
+                chars[ans] = x[i];
+                ans++;
+            }
+            // chars[ans] = chars[i];
+            // ans++;
+        }
+        return ans;
 	}
 };
