032da9de4df9e67643c37b50da05dbb984dc1a58
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-04-08 11:43:47 +0530

    Time: 11 ms (20.76%), Space: 9 MB (19.99%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    map<Node*, Node*> m;
+    Node* cloneGraph(Node* node) {
+        if(node==NULL) return NULL;
+        if(m.find(node) != m.end()) return m[node];
+        Node *ans = new Node(node->val);
+        m[node] = ans;
+        for(int i=0; i<node->neighbors.size(); i++)
+        {
+            ans->neighbors.push_back(cloneGraph(node->neighbors[i]));
+        }
+        return ans;
+    }
+};
