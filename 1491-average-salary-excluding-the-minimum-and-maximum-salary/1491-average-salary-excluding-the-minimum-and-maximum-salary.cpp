78faedc163c41d9f58e186e9004c2d6241fbd8eb
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-05-01 10:06:44 +0530

    Time: 5 ms (23.02%), Space: 7.1 MB (53.11%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -3,17 +3,19 @@ public:
     double average(vector<int>& salary) {
         double ans = 0;
         int n = salary.size();
-        int mn=salary[0];
-        int mx=mn;
+        int mn = salary[0];
+        int mx = mn;
         for(int i=0; i<n; i++)
         {
             ans += salary[i];
             mn = min(mn, salary[i]);
             mx = max(mx, salary[i]);
         }
-        ans -= mn;
-        ans -= mx;
-        ans /= (n-2);
+        // ans -= (mn+mx);
+        // ans -= mx;
+        n -= 2;
+        // ans /= n;
+        ans = (ans-mn-mx)/n;
         return ans;
     }
 };
