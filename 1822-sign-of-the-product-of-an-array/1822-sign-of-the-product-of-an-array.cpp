6453e80a71d7ab660a7385308487788b68cd1aad
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-05-02 13:58:19 +0530

    Time: 8 ms (27.40%), Space: 10.2 MB (86.28%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -5,8 +5,8 @@ public:
         for(int i=0; i<nums.size(); i++)
         {
             if(nums[i] == 0) return 0;
-            ans *= nums[i];
-            if(ans<0) ans = -1;
+            if(nums[i]<0 && ans>0) ans = -1;
+            else if(nums[i]>0 && ans<0) ans = -1;
             else ans = 1;
         }
         return ans;
