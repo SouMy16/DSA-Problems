47d2d989109e972483f4c10f52891c4da755bcd1
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-05-02 13:59:50 +0530

    Time: 3 ms (92.77%), Space: 10.1 MB (86.28%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -7,8 +7,9 @@ public:
             if(nums[i] == 0) return 0;
             if(nums[i]<0) ans++;
         }
-        if(ans%2) ans=-1;
-        else ans=1;
-        return ans;
+        if(ans%2) return -1;
+        // else ans=1;
+        // return ans;
+        return 1;
     }
 };
