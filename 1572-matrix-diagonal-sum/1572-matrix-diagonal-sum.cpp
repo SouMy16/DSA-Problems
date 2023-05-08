866203a1c214b7e6458da9682339acbaeed9bd5b
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-05-08 13:01:21 +0530

    Time: 19 ms (17.77%), Space: 11.2 MB (92.07%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -3,8 +3,13 @@ public:
     int diagonalSum(vector<vector<int>>& mat) {
         int ans = 0;
         int n = mat.size();
-        for(int i=0; i<n; i++) ans+=(mat[i][i]+mat[i][n-1-i]);
-        ans = ans - (n%2==1?mat[n/2][n/2]:0);
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++)
+            {
+                if(i==j) ans += mat[i][j];
+                else if(i+j==n-1) ans+=mat[i][j];
+            }
+        }
         return ans;
     }
 };
