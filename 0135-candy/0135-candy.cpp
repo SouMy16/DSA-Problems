ab384ec418b8bff4881e99d57bf15ac93c60040e
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-05-27 16:36:00 +0530

    Time: 29 ms (27.89%), Space: 17.9 MB (39.03%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,34 +2,17 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        map<int, vector<int>> m;
-        for(int i=0; i<n; i++)
-        {
-            m[ratings[i]].push_back(i);
-        }
         int ans = 0;
-        vector<int> candies(n, 0);
-        for(auto e:m)
+        vector<int> candies(n, 1);
+        for(int i=1; i<n; i++)
         {
-            vector<int> v = e.second;
-            for(int i=0; i<v.size(); i++)
-            {
-                if(v[i]>0 && candies[v[i]-1] != 0 && ratings[v[i]-1] < ratings[v[i]])
-                {
-                    if(v[i]<n-1 && candies[v[i]+1] != 0 && ratings[v[i]+1] < ratings[v[i]])
-                    {
-                        candies[v[i]] = max(candies[v[i]+1], candies[v[i]-1]) + 1;
-                    }
-                    else candies[v[i]] = candies[v[i]-1] + 1;
-                }
-                else if(v[i]<n-1 && candies[v[i]+1] != 0 && ratings[v[i]+1] < ratings[v[i]])
-                {
-                    candies[v[i]] = candies[v[i]+1] + 1;
-                }
-                else candies[v[i]] = 1;
-                ans += candies[v[i]];
-            }
+            if(ratings[i] > ratings[i-1]) candies[i] = candies[i-1]+1;
         }
+        for(int i=n-2; i>=0; i--)
+        {
+            if(ratings[i]>ratings[i+1]) candies[i] = max(candies[i], candies[i+1]+1);
+        }
+        for(int i=0; i<n; i++) ans+=candies[i];
         return ans;
     }
 };
