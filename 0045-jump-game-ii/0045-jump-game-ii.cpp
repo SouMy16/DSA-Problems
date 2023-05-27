61410d457811c5ed14f021c546eef1e9c858e78c
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-05-27 13:26:54 +0530

    Time: 231 ms (36.17%), Space: 18.2 MB (5.74%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int pos=0,n=nums.size();
-        int jumps=0,maxii=0;
-        for(int i=0;i<n;i++)
+        queue<pair<int,int>> q;
+        q.push({0, 0});
+        int n = nums.size();
+        vector<int> vis(n+1, -1);
+        while(!q.empty())
         {
-            if(pos==i) continue;
-            if(nums[i]+i>nums[maxii]+maxii) maxii=i;
-            if(nums[pos]+pos==i || i==n-1)
+            int pos = q.front().first;
+            int cur = q.front().second;
+            q.pop();
+            if(pos >= nums.size()-1) return cur;
+            for(int i=1; i<=nums[pos]; i++)
             {
-                pos=maxii;
-                // i=maxii-1;
-                jumps++;
-                if(pos==n-1) return jumps;
-                // if(pos+nums[pos]>=n-1) return jumps+1;
+                if(pos+i >= nums.size()-1) return cur+1;
+                if(vis[pos+i] == -1)
+                {
+                    vis[pos+i] = 1;
+                    q.push({pos+i, cur+1});
+                }
             }
         }
-        return jumps;
+        return -1;
     }
 };
