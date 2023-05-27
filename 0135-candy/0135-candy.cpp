3bb9a8032c53ea32744899a47b7a2f236a8a5d33
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-05-27 16:39:09 +0530

    Time: 22 ms (71.54%), Space: 17.8 MB (53.69%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
+        // int n = ratings.size();
         int ans = 0;
-        vector<int> candies(n, 1);
-        for(int i=1; i<n; i++)
-        {
-            if(ratings[i] > ratings[i-1]) candies[i] = candies[i-1]+1;
-        }
-        for(int i=n-2; i>=0; i--)
-        {
-            if(ratings[i]>ratings[i+1]) candies[i] = max(candies[i], candies[i+1]+1);
-        }
-        for(int i=0; i<n; i++) ans+=candies[i];
+        vector<int> candies(ratings.size(), 1);
+        for(int i=1; i<ratings.size(); i++) if(ratings[i] > ratings[i-1]) candies[i] = candies[i-1]+1;
+        for(int i=ratings.size()-2; i>=0; i--) if(ratings[i]>ratings[i+1]) candies[i] = max(candies[i], candies[i+1]+1);
+        for(int i=0; i<ratings.size(); i++) ans += candies[i];
         return ans;
     }
 };
