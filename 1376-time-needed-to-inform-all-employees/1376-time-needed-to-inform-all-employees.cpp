30c6a4e3428857ccf8981cfd60b45e14ea2ca0eb
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-06-04 00:03:32 +0530

    Time: 323 ms (59.02%), Space: 121.8 MB (59.75%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int height(int cur, vector<int> &manager, vector<int>& infoT, vector<int> &dp)
+    int height(int cur, vector<vector<int>> &child, vector<int> &infoT)
     {
-        if(cur == -1) return 0;
-        if(dp[cur] != -1) return dp[cur]; 
-        return dp[cur] = infoT[cur] + height(manager[cur], manager, infoT, dp);
+        int mx = 0;
+        for(int i=0; i<child[cur].size(); i++)
+        {
+            mx = max(mx, height(child[cur][i], child, infoT));
+        }
+        return mx + infoT[cur];
     }
     
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         int ans = 0;
-        vector<int> dp(n, -1); 
+        vector<vector<int>> child(n);
         for(int i=0; i<n; i++)
-            ans = max(ans, height(i, manager, informTime, dp));
-        return ans;
+        {
+            if(i!=headID) child[manager[i]].push_back(i);
+        }
+        return height(headID, child, informTime);
     }
 };
