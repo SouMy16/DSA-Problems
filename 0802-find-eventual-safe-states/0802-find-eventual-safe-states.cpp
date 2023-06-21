78f79d1028870e3353aad0505ffc4049b22d837b
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-06-21 23:54:09 +0530

    Time: 172 ms (68.47%), Space: 47.8 MB (77.45%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    bool DFS(int i, vector<vector<int>> &g, vector<int> &vis, map<int, int> &m, vector<int> &dp)
+    bool DFS(int i, vector<vector<int>> &g, vector<int> &vis, vector<int> &dp)
     {
-        if(m[i]) return true;
+        // if(m[i]) return true;
         if(dp[i] != -1) return dp[i] == 1;
         bool ans = true;
         dp[i] = 0;
         for(int j=0; j<g[i].size(); j++)
         {
-            ans &= DFS(g[i][j], g, vis, m, dp);
+            ans &= DFS(g[i][j], g, vis, dp);
         }
         return dp[i] = (ans==true);
     }
     
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        map<int, int> m;
         vector<int> ans;
+        vector<int> dp(n, -1), vis(n, 0);
         for(int i=0; i<n; i++) 
         {
             if(graph[i].size() == 0) 
             {
-                m[i] = 1;
+                dp[i] = 1;
             }
         }
-        vector<int> dp(n, -1), vis(n, 0);
         for(int i=0; i<n; i++)
         {
-            if(DFS(i, graph, vis, m, dp)) ans.push_back(i);
+            if(DFS(i, graph, vis, dp)) ans.push_back(i);
         }
         return ans;
     }
