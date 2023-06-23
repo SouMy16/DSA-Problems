30880e0f7f77d467b49bcbb7de929119c06116ae
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-06-23 17:45:23 +0530

    Time: 386 ms (69.66%), Space: 141.8 MB (70.05%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -6,20 +6,13 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(1001, -1));
         for(int i=0; i<n; i++)
         {
-            for(int j=i+1; j<n; j++)
+            for(int j=0; j<i; j++)
             {
                 int d = nums[j]-nums[i];
-                if(dp[j][500+d] != -1 || dp[i][500+d] != -1) continue;
-                int f = nums[j]+d;
-                int cur = 2;
-                dp[i][500+d]=0;
-                dp[j][500+d]=0;
-                for(int k=j+1; k<n; k++) if(nums[k] == f) dp[k][500+d]=0, cur++, f+=d;
-                if(cur>ans) ans = cur;
-                // f=nums[j], cur-=2;
-                // for(int k=j+1; k<n; k++) if(nums[k] == f) dp[k][500+d] = cur--, f+=d;
+                dp[i][500+d] = max(1+dp[j][500+d], dp[i][500+d]);
+                ans = max(ans, dp[i][500+d]);
             }
         }
-        return ans;
+        return ans+2;
     }
 };
