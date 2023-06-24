188a2a5c069079552868b01671d8f7583815b046
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-06-24 19:55:31 +0530

    Time: 2149 ms (5.29%), Space: 229.7 MB (5.29%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
+    int solve(int ind, int diff, vector<int> &a, map<int, map<int, int>> &m)
+    {
+        if(ind == a.size())
+        {
+            if(diff == 0) return m[ind][diff] = 0;
+            return INT_MIN;
+        }
+        if(m[ind].find(diff) != m[ind].end()) return m[ind][diff];
+        int op1, op2, op3;
+        op1 = solve(ind+1, diff, a, m);
+        op2 = a[ind]+solve(ind+1, diff+a[ind], a, m);
+        op3 = solve(ind+1, diff-a[ind], a, m);
+        return m[ind][diff] = max(op1, max(op2, op3));
+    }
     int tallestBillboard(vector<int>& rods) {
-        int sum = 0;
-        for (int rod : rods) {
-            sum += rod;
-        }
-
-        int dp[sum + 1];
-        dp[0] = 0;
-
-        // Initialize all other elements in dp[] to -1
-        for (int i = 1; i <= sum; i++) {
-            dp[i] = -1;
-        }
-
-        for (int rod : rods) {
-            int dpCopy[sum + 1];
-            // Copy the current state of dp[] to dpCopy[]
-            copy(dp, dp + (sum + 1), dpCopy);
-
-            for (int i = 0; i <= sum - rod; i++) {
-                if (dpCopy[i] < 0) continue;
-
-                // Case 1: Place the current rod on the same height billboard
-                dp[i + rod] = max(dp[i + rod], dpCopy[i]);
-
-                // Case 2: Place the current rod on the taller billboard
-                // Calculate the absolute difference between current height and rod length
-                dp[abs(i - rod)] = max(dp[abs(i - rod)], dpCopy[i] + min(i, rod));
-            }
-        }
-
-        // Return the maximum height of the billboard stored at dp[0]
-        return dp[0];
+        map<int, map<int, int>> m;
+        return solve(0, 0, rods, m);
     }
 };
