2b9885d8e4db584f90a2e32e4113c335a4543d95
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-07-12 15:37:13 +0530

    Time: 164 ms (83.1%), Space: 47.7 MB (81.88%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool DFS(int i, vector<vector<int>> &g, vector<int> &vis, vector<int> &dp)
+    {
+        // if(m[i]) return true;
+        if(dp[i] != -1) return dp[i] == 1;
+        bool ans = true;
+        dp[i] = 0;
+        for(int j=0; j<g[i].size(); j++)
+        {
+            if(!DFS(g[i][j], g, vis, dp)) return dp[i] = false;
+        }
+        return dp[i] = true;
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> ans;
+        vector<int> dp(n, -1), vis(n, 0);
+        for(int i=0; i<n; i++) 
+        {
+            if(graph[i].size() == 0) 
+            {
+                dp[i] = 1;
+            }
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(DFS(i, graph, vis, dp)) ans.push_back(i);
+        }
+        return ans;
+    }
+};
