e37a8a5a7d399b0dc058b6a954fd5aa8645be581
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-10-08 01:49:16 +0530

    Time: 209 ms (20.91%), Space: 10.7 MB (15%) - LeetHub

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    /* let ways[i][j][k] = # ways to construct an array of length i with max element equal to j and a search cost of k. */
-    long long ways[51][101][51];
-    const int MOD = 1e9 + 7;
-    
-    int numOfArrays(int n, int m, int k) {
-		/* There are our base cases. For each index 1 <= j <= m, we require ways[1][j][1] = 1 because the array consisting of only the element j
-			has length 1, maximum element j, and it has a search cost of 1. */
-        for (int j = 1; j <= m; j++) {
-                ways[1][j][1] = 1;
-        }
-        
-        for (int a = 1; a <= n; a++) {
-            for (int b = 1; b <= m; b++) {
-                for (int c = 1; c <= k; c++) {
-                    long long s = 0;
 
-		            /* In this first case, we can append any element from [1, b] to the end of the array. */
-                    s = (s + b * ways[a - 1][b][c]) % MOD;
-                    
-		           /* In this second case, we can append the element "b" to the end of the array. */
-                    for (int x = 1; x < b; x++) {
-						s = (s + ways[a - 1][x][c - 1]) % MOD;
-                    }
-					
-                    ways[a][b][c] = (ways[a][b][c] + s) % MOD;
+    int mod = 1e9+7;
+    int helper(int i, int n, int m, int k,vector<vector<vector<int>>> &dp){
+        if(n<=0){
+            if(k==0) return 1;
+            return 0;
+        }
+
+        if(dp[i][n][k]!=-1) return dp[i][n][k];
+        int ans = 0;
+        for(int j=1;j<=m;j++){
+            if(j>i){
+                if(n>=1 && k>=1){
+                    ans=(ans+helper(j,n-1,m,k-1,dp))%mod;
+                }
+            }else{
+                if(n>=1){
+                    ans=(ans+helper(i,n-1,m,k,dp))%mod;
                 }
             }
         }
 
-        long long ans = 0;
-        for (int j = 1; j <= m; j++) {
-            ans = (ans + ways[n][j][k]) % MOD;
-        }
-        
-        return int(ans);
+
+        return dp[i][n][k] = ans%mod;
+    }
+    int numOfArrays(int n, int m, int k) {
+        vector<vector<vector<int>>> dp(m+1,vector<vector<int>>(n+1,vector<int>(k+1,-1)));
+        return helper(0,n,m,k,dp);
     }
 };
