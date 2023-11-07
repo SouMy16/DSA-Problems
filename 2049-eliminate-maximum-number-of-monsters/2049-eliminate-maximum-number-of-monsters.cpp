908331c71cf56fe1a3efa257342dc26669b4c632
Soumya Agrawal <90271399+SouMy16@users.noreply.github.com>
2023-11-07 10:04:49 +0530

    Time: 113 ms (46.67%), Space: 89.1 MB (29.33%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cpp
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cpp
@@ -2,22 +2,14 @@ class Solution {
 public:
     int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
         int n = dist.size(), ans = 0;
-        priority_queue<vector<double>, vector<vector<double> >, greater<vector<double> > > pq;
+        vector<double> arrivalTime;
         for(int i=0; i<n; i++) {
-            // distance speed time
-            pq.push(vector<double>{1.0*dist[i]/speed[i], 1.0*dist[i], 1.0*speed[i]});
+            arrivalTime.push_back(1.0*dist[i]/speed[i]);
         }
-        int curt = 0;
-        while(!pq.empty()) {
-            int d = (int)pq.top()[1];
-            int s = (int)pq.top()[2];
-            // int t = (int)pq.top()[3];
-            // cout<<d<<" "<<s<<" "<<'\n';
-            d = d - (s*curt);
-            if(d<=0) return ans;
-            ans++;
-            curt++;
-            pq.pop();
+        sort(arrivalTime.begin(), arrivalTime.end());
+        for(int i=0; i<n; i++) {
+            if(arrivalTime[i] > i) ans++;
+            else break;
         }
         return ans;
     }
